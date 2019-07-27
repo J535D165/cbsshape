561df506f6efb556565e78b847b98a787c204765
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-07-27 19:05:24 +0200

    Add level and WGS84 option

--- R/read.R
+++ R/read.R
@@ -3,19 +3,38 @@
 #' Read shapefiles from the CBS Wijk en Buurtkaart with the package sf.
 #'
 #' @param year Read the shape file from this year.
+#' @param level Level of detail of the returned results. Options are
+#'   "gem", "buurt", "wijk". Default "gem".
 #' @param path Local path to the shape files. If NULL, the shapefiles are
 #'   downloaded and a temporary path is created.
+#' @param wgs84 Return results in WGS84 coordinates (CRS 4326). Default
+#'   FALSE (result in Rijksdriehoek Coordinaten).
 #' @param verbose Verbosity. Default TRUE.
 #' @return st object with the CBS Wijk en Buurtkaart
 #' @examples
 #' st_read_cbs(2017)
-st_read_cbs <- function(year, path=NULL, verbose=TRUE) {
+#' st_read_cbs(2018, level="buurt")
+#' st_read_cbs(2018, wgs84=TRUE)
+#' st_read_cbs(2016, path="wijkenbuurt2016/")
+st_read_cbs <- function(year, level="gem", path=NULL, wgs84=FALSE, verbose=TRUE) {
 
   if (is.null(path)){
     path = download_cbs_shapefile(year, verbose=verbose)
   }
 
-  shp <- st_read(file.path(path, sprintf("gem_%s.shp", year)))
+  if (!(level %in% c("gem", "buurt", "wijk"))){
+    stop("level should be 'gem', 'buurt' or 'wijk'")
+  }
+
+  # shapefile path
+  shp_path <- Sys.glob(file.path(path, sprintf("*%s*%s*.shp", level, year)))
+
+  # load shape
+  shp <- st_read(shp_path[[1]])
+
+  if (wgs84){
+    shp <- st_transform(shp, 4326)
+  }
 
   return(shp)
 }
