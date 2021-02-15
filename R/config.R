0f7c4c20a6a9898e67226e38afddda4d2d60c5b9
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-02-15 13:03:07 +0100

    Update CBS open shape file locations

--- R/config.R
+++ R/config.R
@@ -4,12 +4,13 @@ shapefiles <- c(
   "2011"="http://download.cbs.nl/regionale-kaarten/2011-buurtkaart-shape-versie-3.zip",
   "2012"="http://download.cbs.nl/regionale-kaarten/shape-2012-versie-3.0.zip",
   "2013"="http://download.cbs.nl/regionale-kaarten/shape-2013-versie-3-0.zip",
-  "2014"="https://www.cbs.nl/-/media/_pdf/2016/35/shape%202014%20versie%2030.zip",
+  "2014"="https://www.cbs.nl/-/media/_pdf/2016/35/shape-2014-versie-30.zip",
   "2015"="https://www.cbs.nl/-/media/_pdf/2017/36/buurt_2015.zip",
-  "2016"="https://www.cbs.nl/-/media/cbs/dossiers/nederland%20regionaal/wijk-en-buurtstatistieken/shape%202016%20versie%2030.zip", 
-  "2017"="https://www.cbs.nl/-/media/cbs/dossiers/nederland%20regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2017_v3.zip",
-  "2018"="https://www.cbs.nl/-/media/cbs/dossiers/nederland%20regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2018_v2.zip",
-  "2019"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2019_v1.zip"
+  "2016"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/shape-2016-versie-30.zip", 
+  "2017"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2017_v3.zip",
+  "2018"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2018_v3.zip",
+  "2019"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2019_v2.zip",
+  "2020"="https://www.cbs.nl/-/media/cbs/dossiers/nederland-regionaal/wijk-en-buurtstatistieken/wijkbuurtkaart_2020_v1.zip"
   
 )
 
